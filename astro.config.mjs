1826f9a6fa65796f847b134ad817ce82682071bd
Ty <leslie@Tylahs-MacBook-Pro.local>
2026-05-12 12:04:34 -0400

    Fix: remove Vercel adapter (not needed)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   output: 'static',
   integrations: [tailwind()],
-  adapter: vercel(),
 });
